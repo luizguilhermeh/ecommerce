da447ea3525063c7dc26d5a5507bd4fc492dada6
Luiz Guilherme da Costa <luizguilhermeh@yahoo.com.br>
2022-08-01 15:55:24 -0300

    Update

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function removeCartItem(event) {
 //Quantidade Modificadas
 function quantityChanged(event) {
   var input = event.target;
-  if(isNaN(input.value) || input.value <= 0) {
+  if (isNaN(input.value) || input.value <= 0) {
     input.value = 1;
   }
   updatetotal();
